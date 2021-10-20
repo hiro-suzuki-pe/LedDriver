4ecb1e208eb5590d94ed07e62fedf3c2354a5194
Hiroyuki Suzuki <suzuki.hiroyuki@mbr.nifty.com>
2021-10-21 04:34:51 +0900

    RuntimeError実装

--- Test/mocks/RuntimeErrorStub.c
+++ Test/mocks/RuntimeErrorStub.c
@@ -1,7 +1,6 @@
 #include "RuntimeErrorStub.h"
 
-//static const char *message = "No Error";
-static const char *message = "LED Driver: out-of-bounds LED";
+static const char *message = "No Error";
 static int parameter = -1;
 static const char *file = 0;
 static int line = -1;
